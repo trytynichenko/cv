5958dd116445b67e01c079c71dfde7756b7ff01f
Leonid Trytynichenko <leo@trytynichenko.com>
2018-04-26 13:57:35 +0300

    fix spelling

--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -6,7 +6,7 @@
         My name is <strong>{{ name }}</strong> and I am a Front-end (React) software engineer.
         I have {{ experience }}+ years of experience in e-commerce building beautiful interfaces
         and providing responsive front-end development and WordPress websites for the clients
-        from all over the world. As a team lead, I take responsibility of providing guidance,
+        from all over the world. As a team lead, I take the responsibility for providing guidance,
         instruction, direction, and leadership to the team for the purpose of reaching the business
         goals and achieving successful results.
       </h2>
