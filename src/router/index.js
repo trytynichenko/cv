956a341c64718156fc4e92daa882cd5d852fcc92
Leonid Trytynichenko <leo@trytynichenko.com>
2018-04-26 10:36:51 +0300

    add GA to vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import VueRouter from 'vue-router';
+import ga from 'vue-ga';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
 import Contact from '@/views/Contact.vue';
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
+const router = new VueRouter({
   routes: [
     { path: '/', name: 'home', component: Home },
     { path: '/about', name: 'about', component: About },
     { path: '/contact', name: 'contact', component: Contact },
   ],
 });
+
+ga(router, 'UA-50810083-1');
+
+export default router;
